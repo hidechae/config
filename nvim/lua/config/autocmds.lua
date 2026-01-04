e4fd6ebfb65da9c19beb76fdcffe7fa6fee6f76a
Hideyuki Utsunomiya <hidechae@gmail.com>
2026-01-04 20:23:38 +0900

    disable vim spell check

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,6 +7,9 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Disable LazyVim's automatic spell check for certain filetypes
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
 -- Set fish files as shell filetype
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.fish",
